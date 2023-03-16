f2aec9f3eb7317e9aa0e075c6ef4a3c789933526
sejalkothe <sejalkothe2001@gmail.com>
2023-03-16 16:28:11 +0530

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import React from 'react';
+
+import SurveyTable from './Components/Survey/SurveyTable/surveyTable';
+
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+    <SurveyTable />
+  </div>
+  );
 }
 
 export default App;
